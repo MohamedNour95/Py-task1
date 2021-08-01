22bb2251cb00790f01dfb71603098c12e835c671
MohamedNour95 <87098577+MohamedNour95@users.noreply.github.com>
2021-08-01 09:16:08 +0200

    Add files via upload
    Ask the user to enter the radius of a circle in order to alert its calculated area and circumference.

---
+++ problem9.py
@@ -0,0 +1,6 @@
+PI = 3.14
+radius = float(input('Please enter the radius of the circle:'))
+circumference = 2 * PI * radius
+area = PI * radius * radius
+print("Circumference Of the Circle = "+ str(circumference))
+print("Area Of the Circle = "+ str(area))
