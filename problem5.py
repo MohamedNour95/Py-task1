9558eabd2bff44f33bfff83f4d60573a4f97678e
MohamedNour95 <87098577+MohamedNour95@users.noreply.github.com>
2021-08-10 09:47:25 +0200

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -1,6 +1,25 @@
-def splitstring(value):
-    string1, string2 = value[:len(value)//2], value[len(value)//2:]
-    return string1, string2
-mystring = "anything" #put your string here
-print('My string',mystring)
-print('Split the string into two:',splitstring(mystring))
+string = ("mohamed") #Please enter your string here
+
+def split_str(string):
+    str_len = len(string)
+    
+    if str_len % 2 == 0 :
+        half = str_len / 2
+        i = 0
+        for c in range(len(string)):
+            if i % half == 0:
+              print('')
+              print(string[c])
+              i += 1
+                
+    else:
+        half = str_len / 2
+        first_half = round(half)
+        i = 0
+        for c in range(len(string)) :
+            if i % first_half == 0 :
+              print('')
+            print(string[c])
+            i += 1
+
+split_str(string)
